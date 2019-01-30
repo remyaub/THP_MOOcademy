6fce21974d113122577988f0bcdba3fe9216c94b
RemyAu <centratobey@gmail.com>
2019-01-30 16:55:39 +0100

    adding full project to git

---
+++ MOOcademy/db/migrate/20190130145413_create_cours.rb
@@ -0,0 +1,10 @@
+class CreateCours < ActiveRecord::Migration[5.2]
+  def change
+    create_table :cours do |t|
+      t.text :title
+      t.text :description
+
+      t.timestamps
+    end
+  end
+end
